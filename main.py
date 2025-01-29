ed4175250c7a4f0ec69162d814da95ce6f26383a
Shaheryarkhalid <shaheryar.150ash@gmail.com>
2025-01-29 11:21:41 +0400

    Game logic added

--- main.py
+++ main.py
@@ -1,19 +1,55 @@
 import pygame
+from asteroid import Asteroid
 from constants import *
+from player import Player
+from asteroidfield import AsteroidField
+from shot import Shot
+
+clock = pygame.time.Clock()
+dt = 0
 
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    global dt
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    fired_shots = pygame.sprite.Group()
+    Shot.containers = (fired_shots, drawable, updatable)
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
+    asteroidfield = AsteroidField()
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(pygame.Color(0, 0, 0))
+
+        for obj in updatable:
+            obj.update(dt)
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        for asteroid_obj in asteroids:
+            if asteroid_obj.colliding(player):
+                return
+            for shot_obj in fired_shots:
+                if asteroid_obj.colliding(shot_obj):
+                    asteroid_obj.kill()
+                    shot_obj.kill()
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
